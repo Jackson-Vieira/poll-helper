bb5d69a652c05c0f79e257ffc50d331040e4895e
Jackson <jacksonvieiira12@gmail.com>
2022-10-18 23:50:41 -0300

    v1 sem API

--- polls/admin.py
+++ polls/admin.py
@@ -1,21 +1,20 @@
 from django.contrib import admin
 
-from polls.models import Choice, Question
+from polls.models import Choice, Topic, Registry
 
 # Register your models here.
 class ChoiceInline(admin.TabularInline):
     model = Choice
-    extra = 0
+    extra = 1
 
-class QuestionAdmin(admin.ModelAdmin):
+class TopicAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None, {'fields':['question_text']}),
-        ('Data informations', {'fields':['pub_date']}),   
-        ('Privacity informations', {'fields':['private_question']})
+        ('Privacidade', {'fields':['owner']}),
+        (None, {'fields':['topic_text']}),
     ]
 
     list_display = ['question_text', 'pub_date', 'was_published_recently']
-    list_filter = ['pub_date']
+    list_filter = ['created']
     search_fields = ['question_text']
 
     date_hierarchy = 'pub_date'
@@ -23,4 +22,5 @@ class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline,]
 
 
-admin.site.register(Question, QuestionAdmin)
+admin.site.register(Topic, TopicAdmin)
+admin.site.register(Registry)
--- polls/models.py
+++ polls/models.py
@@ -1,41 +1,45 @@
 from django.db import models
-from django.utils import timezone
-from django.contrib import admin
 from django.contrib.auth.models import User
 
-import datetime
 import uuid
 
-class Question(models.Model):
+class Topic(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False, unique=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=False)
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    question_text = models.CharField(max_length=200, blank=False, null=False)
-    pub_date = models.DateTimeField('date published', blank=False, null=False)
-    private_question = models.BooleanField('private question', blank=False, null=False, default=False)
+    topic_text = models.CharField(max_length=200, blank=False, null=False)
+    created = models.DateTimeField('creation date', auto_now_add=True)
+    updated = models.DateField('last update date', auto_now=True)
 
-
-    @admin.display(
-        boolean=True,
-        ordering='pub_date',
-        description='Published recently?'
-    )
-    def was_published_recently(self):
-        datetime_now = timezone.now()
-        return datetime_now - datetime.timedelta(days=1) <= self.pub_date <= datetime_now
-    
     def there_choice(self):
         return len(self.choice_set.all()) >= 1
 
     def __str__(self):
         return self.question_text
+    
+    class Meta:
+        verbose_name = 'Topic'
+        verbose_name_plural = 'Topics'
+        ordering = ['topic_text']
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0) #  Change to ONE-TO-MANY
-    
+    question = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='question', blank=False, null=False)
+    choice_text = models.CharField(max_length=200, blank=False, null=False)
+    registrys = models.ManyToManyField(User, through='Registry', related_name='registrys')
+    # votes = models.IntegerField(default=0) #  Change to ONE-TO-MANY
+    # image 
+
     def __str__(self):
         return self.choice_text
 
     class Meta:
-        ordering = ["-votes"]
+        verbose_name = 'Choice'
+        verbose_name_plural = 'Choices'
+        
+class Registry(models.Model):
+    user =  models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='registry', blank=False, null=False)
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE, related_name='registry', blank=False, null=False)
+    created = created = models.DateTimeField('creation date', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Registry'
+        verbose_name_plural = 'Registrys'
